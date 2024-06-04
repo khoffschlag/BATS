725de9dd2046dbacafd023a109a67b6f4aff6d9f
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2024-06-04 18:48:21 +0200

    Generate needed components home, about, theory, exercise and results

---
+++ src/app/exercise/exercise.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercise.component.html',
+  styleUrl: './exercise.component.css'
+})
+export class ExerciseComponent {
+
+}
---
+++ src/app/theory/theory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theory',
+  standalone: true,
+  imports: [],
+  templateUrl: './theory.component.html',
+  styleUrl: './theory.component.css'
+})
+export class TheoryComponent {
+
+}
