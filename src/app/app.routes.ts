4f322a510b32209d0128b2e2e8e0e18d158f9045
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2024-06-04 18:55:43 +0200

    Add basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OverviewComponent } from './overview/overview.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+        title: "Home"
+    },
+    {
+        path: "overview",
+        component: OverviewComponent,
+        title: "Overview"
+    },
+    {
+        path: "about",
+        component: AboutComponent,
+        title: "About us"
+    }
+];
