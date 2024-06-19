9b56e79047647e68eff576d5e9445ee35cf667ae
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2024-06-20 00:01:10 +0200

    Add binary to decimal exercise

--- src/app/exercise-data.model.ts
+++ src/app/exercise-data.model.ts
@@ -5,8 +5,8 @@ export class ExerciseData {
     title: string = '';
     task: string = '';
 
-    userAnswer: number[] = Array(8).fill(0);
-    targetAnswer: number[] = Array(8).fill(-1);
+    userAnswer: number | number[] = Array(8).fill(0);
+    targetAnswer: number | number[] = Array(8).fill(-1);
     
     result: boolean = false;
     feedback: string = '';
