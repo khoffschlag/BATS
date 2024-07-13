b6bfe069d1a26cd89ec843d3581759ac8995ea7a
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2024-07-13 18:03:18 +0200

    Add missing subscribe

--- backend/server.js
+++ backend/server.js
@@ -125,7 +125,15 @@ app.get("/api/is-authenticated", isAuthenticated, (req, res) => {
 // Update correctAnswerStreak value
 app.post("/api/update-streak", async (req, res) => {
     try {
-        const { username, correctAnswerStreak } = req.body;
+
+        if (!req.session.user || !req.session.user.username) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const username = req.session.user.username;
+        const correctAnswerStreak = req.body.correctAnswerStreak;
+        console.log(`Xusername: ${username}`);
+        console.log(`XcorrectAnswerStreak: ${correctAnswerStreak}`);
         const user = await User.findOne({ username });
 
         if (!user) {
